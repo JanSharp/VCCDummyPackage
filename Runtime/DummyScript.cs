76dd9904d8375eab1904c2111485990d768e056b
JanSharp <steinmeister36@gmail.com>
2023-06-28 19:42:54 +0200

    Add assembly definition and dummy script

---
+++ Runtime/DummyScript.cs
@@ -0,0 +1,16 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class DummyScript : UdonSharpBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello World!");
+        }
+    }
+}
